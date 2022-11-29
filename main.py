50051bf98abd5a42e0f5099c7a0857b5e9646ef7
Daniel Kiefer <danielfmpinho@gmail.com>
2022-11-29 15:51:25 -0300

    changes

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-print("Welcome to Overdrive!!")
-name = input("Please enter your name:")
-age = input("What is your age?")
-
-print('Hello', name, 'you are', age, 'years old.')
+print("Welcome to my first game!")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
 health = 10
 
 if age >= 18:
     print("You are old enough to play!")
 
-    wants_to_play = input("Do you want to play?").lower()
+    wants_to_play = input("Do you want to play? ").lower()
     if wants_to_play == "yes":
-        print("You are starting with", health, "health")
+        print("You are staring with", health, "health")
         print("Let's play!")
 
-        left_or_right = input(
-            "First choice... Left or Right (left/right)?").lower()
+        left_or_right = input("First choice... Left or Right (left/right)? ")
         if left_or_right == "left":
-            ans = input(
-                "Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)?").lower()
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)? ")
 
             if ans == "around":
                 print("You went around and reached the other side of the lake.")
-
             elif ans == "across":
-                print(
-                    "You managed to get across, but were bit by a fish and lost 5 health.")
+                print("You managed to get across, but were bit by a fish and lost 5 health.")
                 health -= 5
 
-            ans = input(
-                "You notice a house and a river. Which do you go to (river/house)?").lower()
+            ans = input("You notice a house and a river. Which do you go to (river/house)? ")
             if ans == "house":
-                print(
-                    "You go to the house and are greted by the owner... He doesn't like you and you lose 5 health.")
+                print("You go to the house and are greeted by the owner... He doesn't like you and you lose 5 health")
                 health -= 5
 
                 if health <= 0:
@@ -43,12 +35,23 @@ if age >= 18:
             else:
                 print("You fell in the river and lost...")
 
+
         else:
             print("You fell down and lost...")
 
     else:
         print("Cya...")
-
-
 else:
-    print("You are not old enought to play...")
+    print("You are not old enough to play...")
+
+
+
+
+
+
+
+
+
+
+
+
